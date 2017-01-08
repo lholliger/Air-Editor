63da6e3e3135bd6fb73c7369d36c8fc9d429f096
DatOneLefty <k9dogs101@gmail.com>
2017-01-07 23:06:48 -0500

    modified:   README.md 	modified:   js/air.js 	deleted:    air.js

--- README.md
+++ README.md
@@ -1,15 +1,17 @@
 # Air-Editor
 A super light text editor written in PHP
-
+##How to
+1. clone and go to the Air Editor directory
+2. run the command ./air
+3. try to use what there is (super buggy)
 ##Dependencies
 1. Google Chrome
-2. PHP (soon to be required)
 3. UNIX-based OS
 
 
 ##Todo
 1. Settings
-2. Actual use
+2. Actual use (entering characters?)
 3. Saving files
 4. Themes
 5. Simple installer
--- js/air.js
+++ js/air.js
@@ -7,6 +7,25 @@ String.prototype.replaceAt=function(index, character) {
     return this.substr(0, index) + character + this.substr(index + 1);
 }
 
+if (!String.prototype.splice) {
+    /**
+     * {JSDoc}
+     *
+     * The splice() method changes the content of a string by removing a range of
+     * characters and/or adding new characters.
+     *
+     * @this {String}
+     * @param {number} start Index at which to start changing the string.
+     * @param {number} delCount An integer indicating the number of old chars to remove.
+     * @param {string} newSubStr The String that is spliced in.
+     * @return {string} A new string with the spliced substring.
+     */
+    String.prototype.splice = function(start, delCount, newSubStr) {
+        return this.slice(0, start) + newSubStr + this.slice(start + Math.abs(delCount));
+    };
+}
+
+
 
 function on(line, column) {
 var a =  document.getElementById(line).innerHTML;
@@ -45,14 +64,26 @@ function runkey(code) {
 	if (code == 38) {
 		sel((l - 1),c);
 	}
-	if (code == 40) {
+	else if (code == 40) {
 		sel((l + 1),c);
 	}
-	if (code == 37) {
+	else if (code == 37) {
 		sel(l,c - 1);
 	}
-	if (code == 39) {
+	else if (code == 39) {
 		sel(l,c + 1);
+	}
+	else if (code == 8) {
+		off(l,c)
+		str = document.getElementById(l).innerHTML;
+		document.getElementById(l).innerHTML = str.slice(0, c-1) + str.slice(c);
+		sel(l, c - 1);
+	}
+	else {
+		off(l, c);
+		a = document.getElementById(l).innerHTML;
+		document.getElementById(l).innerHTML = a.splice(c, c, String.fromCharCode(code));
+		sel(l, c + 1);
 	}
 }
 
